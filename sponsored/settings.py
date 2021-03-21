852ef4e090eae447f11cba1836bc903820d943df
HarshKakran <harshkakran05@gmail.com>
2021-03-22 00:36:18 +0530

    Deploy

--- sponsored/settings.py
+++ sponsored/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['https://sponsored-app.herokuapp.com/']
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -82,13 +83,23 @@ WSGI_APPLICATION = 'sponsored.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sponsored',
+        'USER': 'postgres',
+        'PASSWORD': 'database',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -139,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ekantyadav001@gmail.com'
-EMAIL_HOST_PASSWORD = 'ohntgsyuisgqdzlu'
+EMAIL_HOST_USER = 'harshkakran05@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 
 django_heroku.settings(locals())
